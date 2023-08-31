a7a4ff69ed13565d731beb004366598c7e3d78dc
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2023-08-31 21:35:51 +0530

    ci integration

--- build.sh
+++ build.sh
@@ -6,8 +6,4 @@ UPDATE_CACHE=""
 echo master_deploy.sh . >>.dockerignore
 echo buildenv.sh . >>.dockerignore
 echo awsconfiguration.sh . >>.dockerignore
-# docker-compose -f docker/docker-compose.yml build $APP_NAME
-# docker create --name app $APP_NAME:latest
-#sed -i s/@env@/$ENV/g docker/Dockerfile
-docker build --no-cache=true -f docker/Dockerfile \
-   -t $APP_NAME:latest .
+docker build --no-cache=true -t $APP_NAME:latest .
